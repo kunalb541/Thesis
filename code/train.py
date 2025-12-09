4871a67e588d45c5fad016e108148cb1f7900d3c
Kunal Bhatia <32597766+kunalb541@users.noreply.github.com>
2025-12-09 19:44:03 +0100

    Update train.py

--- code/train.py
+++ code/train.py
@@ -41,7 +41,7 @@ import warnings
 try:
     current_dir = Path(__file__).resolve().parent
     sys.path.insert(0, str(current_dir))
-    from causal_hybrid_model import CausalHybridModel, CausalConfig
+    from transformer import CausalHybridModel, CausalConfig
 
     def count_parameters(model):
         return sum(p.numel() for p in model.parameters() if p.requires_grad)
